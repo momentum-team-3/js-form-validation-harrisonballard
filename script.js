495cb00a90c15f45c9742e9a57e744305fde3106
Harrison Ballard <harrisonwballard@gmail.com>
2020-07-27 13:24:02 -0400

    first pass

--- script.js
+++ script.js
@@ -1,1 +1,190 @@
 console.log('Add validation!');
+
+function validateName () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#name");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("name input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("name input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
+
+function validateCarYear () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#car-year");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("car year input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("car year input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
+
+function validateCarMake () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#car-make");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("car make input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("car make input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
+
+function validateCarModel () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#car-model");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("car model input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("car model input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
+
+function validateStartDate () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#start-date");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("start date input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("start date input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
+
+function validateDays () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#days");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("number of days input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("number of days input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
+
+function validateCC () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#credit-card-field");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("credit card number input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("credit card number input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
+
+function validateCVV () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#cvv");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("cvv input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("cvv input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
+
+function validateExpiration () {
+    //validate presence, check if value, and if has any text
+    const nameInput = document.querySelector("#expiration");
+    const name = nameInput/value;
+    const parentEl = nameInput.parentElement;
+    
+    //next one will eval as true if value of name is anything besides an empty string
+    if (name) {
+    
+    Console.log("expiration input is valid");
+    parentEl.classList.remove("input-invalid");
+    parentEl.classList.add("input-valid");
+    }
+    else {
+    Console.log("expiration input is invalid");
+    parentEl.classList.remove("input-valid");
+    parentEl.classList.add("input-invalid");
+    markFormAsInvalid();
+    }
+}
